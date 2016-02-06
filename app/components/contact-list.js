cd8f2c99719a5249eaebde0af67c689ed8df73d8
Leo Hernandez <leo@leojh.com>
2016-02-06 11:10:49 -0500

    another commit

--- app/components/contact-list.js
+++ app/components/contact-list.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  filteredPeople: function() {
+    const filter = this.get('filter');
+    const people = this.get('people');
+
+    if (!filter) {return people;}
+
+    return people
+      .filter(p => p.toLowerCase()
+        .includes(filter.toLowerCase()
+      ));
+
+  }.property('filter'),
+
+  filterObs: function () {
+    console.log(this.get('filter'))
+  }.observes('filter')
+
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -3,13 +3,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model: function() {
-    return [
-    'Leo',
-    'Mike',
-    'Bob',
-    'Alice'
-    ];
-  }
+    const store = this.get('store')
 
+    store.createRecord('person',{first: 'leo', last: 'hernandez'})
+    store.createRecord('person',{first: 'bob', last: 'smith'})
+    store.createRecord('person',{first: 'alice', last: 'alice'})
+
+    return store.all('person')
+  },
+
+  actions: {
+    saveAll: function() {
+      this.store.all('person')
+        .forEach(p => p.save())
+    }
+  }
 
 });
